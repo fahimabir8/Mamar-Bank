8e3d6e6f072ea24017c67f59dda028355513fd71
fahimabir8 <iamabir444@gmail.com>
2024-07-26 12:18:46 +0600

    Revert changes 01

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -140,14 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATIC_URL = 'static/'
-
 
 
 # Default primary key field type
